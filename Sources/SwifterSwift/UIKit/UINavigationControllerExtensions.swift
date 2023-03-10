773e91727bad80253097e2414099d2d4ff9a9df7
Ryan McKinney <rydamckinney@gmail.com>
2023-03-10 13:03:26 -0500

    Hide public extension on navigation controller

--- Sources/SwifterSwift/UIKit/UINavigationControllerExtensions.swift
+++ Sources/SwifterSwift/UIKit/UINavigationControllerExtensions.swift
@@ -11,26 +11,26 @@ public extension UINavigationController {
     /// - Parameters:
     ///   - animated: Set this value to true to animate the transition (default is true).
     ///   - completion: optional completion handler (default is nil).
-    func popViewController(animated: Bool = true, _ completion: (() -> Void)? = nil) {
-        // https://github.com/cotkjaer/UserInterface/blob/master/UserInterface/UIViewController.swift
-        CATransaction.begin()
-        CATransaction.setCompletionBlock(completion)
-        popViewController(animated: animated)
-        CATransaction.commit()
-    }
+//    func popViewController(animated: Bool = true, _ completion: (() -> Void)? = nil) {
+//        // https://github.com/cotkjaer/UserInterface/blob/master/UserInterface/UIViewController.swift
+//        CATransaction.begin()
+//        CATransaction.setCompletionBlock(completion)
+//        popViewController(animated: animated)
+//        CATransaction.commit()
+//    }
 
     /// SwifterSwift: Push ViewController with completion handler.
     ///
     /// - Parameters:
     ///   - viewController: viewController to push.
     ///   - completion: optional completion handler (default is nil).
-    func pushViewController(_ viewController: UIViewController, completion: (() -> Void)? = nil) {
-        // https://github.com/cotkjaer/UserInterface/blob/master/UserInterface/UIViewController.swift
-        CATransaction.begin()
-        CATransaction.setCompletionBlock(completion)
-        pushViewController(viewController, animated: true)
-        CATransaction.commit()
-    }
+//    func pushViewController(_ viewController: UIViewController, completion: (() -> Void)? = nil) {
+//        // https://github.com/cotkjaer/UserInterface/blob/master/UserInterface/UIViewController.swift
+//        CATransaction.begin()
+//        CATransaction.setCompletionBlock(completion)
+//        pushViewController(viewController, animated: true)
+//        CATransaction.commit()
+//    }
 
     /// SwifterSwift: Make navigation controller's navigation bar transparent.
     ///
